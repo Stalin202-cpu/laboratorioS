01f55ea8445d6410750b696d987b14253df8039d
stalin <stalinjaime@gmail.com>
2025-05-22 16:36:35 -0500

    Views

--- Aplicaciones/Empleado/views.py
+++ Aplicaciones/Empleado/views.py
@@ -5,3 +5,43 @@ from django.contrib import messages
 def inicio(request):
     listadoEmpleado=Empleado.objects.all()
     return render(request,"inicioem.html",{'empleado':listadoEmpleado})
+
+def nuevoEmpleado(request):
+    return render(request,"nuevoEmpleado.html")
+#Almacenando los datos de cargo en la Bdd
+def guardarEmpleado(request):
+    nombre = request.POST["nombre"]
+    ubicacion = request.POST["ubicacion"]
+    telefono = request.POST["telefono"]
+    nuevoEmpleado=Empleado.objects.create(
+            nombre=nombre,
+            ubicacion=ubicacion,
+            telefono=telefono,
+        )
+    #mensaje de confirmacion
+    messages.success(request,"Empleado guardado exitosamente")
+    return redirect('inicioem')
+def eliminarEmpleado(request,id):
+    empleadoEliminar = Empleado.objects.get(id=id)
+    empleadoEliminar.delete()
+    messages.success(request,"Empleado ELIMINADO exitosamente")
+    return redirect('inicioem')
+
+#editar
+def editarEmpleado(request,id):
+    empleadoEditar=Empleado.objects.get(id=id)
+    return render(request,"editarEmpleado.html",{'empleadoEditar':empleadoEditar})
+
+def procesarEdicionEmpleado(request):
+    id=request.POST["id"]
+    nombre = request.POST["nombre"]
+    ubicacion = request.POST["ubicacion"]
+    telefono = request.POST["telefono"]
+    empleado=Empleado.objects.get(id=id)
+    empleado.nombre=nombre
+    empleado.ubicacion=ubicacion
+    empleado.telefono=telefono
+    empleado.save()
+    #mensaje de confirmacion
+    messages.success(request,"Empleado ACTUALIZADO exitosamente")
+    return redirect('inicioem')
